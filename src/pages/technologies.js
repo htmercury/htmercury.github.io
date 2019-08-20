b77db047b57232eb14418aafd8914e672105a7cd
Ka Wong <kawong2020@u.northwestern.edu>
2019-08-20 02:06:59 -0500

    add technologies

--- src/pages/technologies.js
+++ src/pages/technologies.js
@@ -1,9 +1,25 @@
 import React from "react"
-import { Link } from "gatsby"
+import classnames from "classnames"
 
 import genStyles from "../styles/page.module.scss"
+import styles from "../styles/technologies.page.module.scss"
 
-import Image from "../components/image"
+// 1
+import babelUrl from "../assets/babel.svg"
+import webpackUrl from "../assets/webpack.svg"
+import nodeUrl from "../assets/node.svg"
+import yarnUrl from "../assets/yarn.svg"
+import npmUrl from "../assets/npm.svg"
+// 2
+import reactUrl from "../assets/react.svg"
+// 3
+import htmlUrl from "../assets/html5.svg"
+import cssUrl from "../assets/css3.svg"
+// 4
+import gatsbyUrl from "../assets/gatsby.svg"
+// 5
+import netcoreUrl from "../assets/NET_Core.svg"
+
 import SEO from "../components/seo"
 
 import { Location } from "@reach/router"
@@ -12,30 +28,71 @@ const TechnologiesPage = ({ location }) => (
   <>
     <SEO title="Technologies" />
     <div className={genStyles.title}>Technologies.</div>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget mollis
-      nisi. Ut eget odio at metus mollis convallis. Nam lacinia iaculis tortor
-      nec scelerisque. Etiam in est vel erat luctus ultrices non et arcu. In
-      bibendum dui vel ex fermentum mollis. In pulvinar, leo volutpat placerat
-      euismod, urna eros vulputate massa, et egestas sem velit quis arcu.
-      Praesent erat enim, pellentesque et enim et, rutrum lobortis nulla. Nulla
-      facilisi. Fusce vel libero tristique elit luctus molestie. Vestibulum
-      iaculis libero eu metus fermentum bibendum. Nulla tincidunt mi et
-      malesuada luctus. Donec molestie ut ipsum ac mollis. Vivamus a semper
-      elit, nec fringilla turpis. Proin dignissim sapien vel dui varius gravida.
-      Suspendisse aliquam faucibus ex egestas pharetra. Aenean non sodales mi.
-      Donec porta leo quis egestas hendrerit. Mauris ornare, odio vitae
-      facilisis pulvinar, metus enim fringilla elit, nec sagittis enim risus eu
-      mauris. Sed auctor orci hendrerit nisl dictum viverra. Praesent rhoncus,
-      libero vitae scelerisque facilisis, eros lorem faucibus tortor, sed
-      lacinia sapien purus elementum erat. Vestibulum et maximus diam, sit amet
-      aliquet felis. Donec ultricies, diam et interdum ullamcorper, tortor neque
-      sollicitudin nisl, id condimentum odio eros a nunc.
-    </p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className={styles.techGrid}>
+      <div>
+        <div className={classnames(genStyles.container, styles.tech)}>
+          <div>
+            <div className={styles.movingWrapper}>
+              <img src={babelUrl} alt="babel"></img>
+              <img src={webpackUrl} alt="webpack"></img>
+              <img src={nodeUrl} alt="node"></img>
+              <img src={yarnUrl} alt="yarn"></img>
+              <img src={npmUrl} alt="npm"></img>
+            </div>
+          </div>
+          <div></div>
+        </div>
+        <div className={classnames(genStyles.container, styles.tech)}>
+          <div>
+            <div className={styles.spinningWrapper}>
+              <img src={reactUrl} alt="react"></img>
+              React
+            </div>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div>
+        <div className={classnames(genStyles.container, styles.tech)}>
+          <div>
+            <div className={styles.htmlCssWrapper}>
+              <div>
+                <div className={styles.htmlCssImg}>
+                  <img src={htmlUrl} alt="html"></img>
+                  <img src={cssUrl} alt="css"></img>
+                </div>
+                <div>HTML</div>
+                <div>CSS</div>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </div>
+        <div className={classnames(genStyles.container, styles.tech)}>
+          <div>
+            <img
+              className={styles.genWrapper}
+              src={gatsbyUrl}
+              alt="gatsby"
+            ></img>
+          </div>
+          <div></div>
+        </div>
+      </div>
+      <div>
+        <div className={classnames(genStyles.container, styles.tech)}>
+          <div>
+            <img
+              className={styles.coreWrapper}
+              src={netcoreUrl}
+              alt="netcore"
+            ></img>
+          </div>
+          <div></div>
+        </div>
+        <div></div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </>
 )
 
