ce42d15f3c66ee7dc257b58539dc32f5b6c5c780
Ka Wong <kawong2020@u.northwestern.edu>
2019-10-03 21:00:22 -0500

    add cname plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,6 +37,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-layout`,
+    'gatsby-plugin-cname',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
